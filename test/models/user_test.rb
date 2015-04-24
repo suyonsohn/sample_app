abbea754b3c39c7b1e8db44fe576bed65d3023bf
Su Yon Sohn <suyonsohn@gmail.com>
2015-04-23 18:17:21 -0700

    add user model validation test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name: "Kim", email: "kim@kim.ca")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
