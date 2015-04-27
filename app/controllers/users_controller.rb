4013d51888ba6f32304f792990ac79503c95ab61
Su Yon Sohn <suyonsohn@gmail.com>
2015-04-27 11:05:39 -0700

    add create users action, error messages partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     # debugger
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
