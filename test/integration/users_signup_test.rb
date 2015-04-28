893040e27079e409a468617366032f45a1e0a865
Su Yon Sohn <suyonsohn@gmail.com>
2015-04-27 18:01:20 -0700

    add more signup tests, refactor flash block

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup information" do 
@@ -26,7 +28,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/show'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    assert_not flash.empty?
   end
 end
