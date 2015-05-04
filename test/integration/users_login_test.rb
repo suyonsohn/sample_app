2e3c7a2f1098bc1cf0812f422a62516bb6afb1fe
Su Yon Sohn <suyonsohn@gmail.com>
2015-05-04 12:30:36 -0700

    test layout changes upon login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     # Visit the login path.
     get login_path
@@ -16,6 +20,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     # Verify that the flash message doesn't appear on the new page.
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    # Visit the login path.
+    get login_path
+    # Post valid information to the sessions path.
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    # Verify that the login link disappears.
+    assert_select "a[href=?]", login_path, count: 0
+    # Verify that a logout link appears.
+    assert_select "a[href=?]", logout_path
+    # Verify that a profile link appears.
+    assert_select "a[href=?]", user_path(@user)
   end
 
 end
