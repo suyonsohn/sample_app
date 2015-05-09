2c84761cd26b1f77de3793ba99e1d57b2d9e94fb
Su Yon Sohn <suyonsohn@gmail.com>
2015-05-09 16:54:59 -0700

    log in with remember me option

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -7,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
+      remember user
       redirect_to user
     else
       # Create an error message.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  
+  attr_accessor :remember_token
 
   # Use callback to standardize on all lower-case addresses
   # self.email = self.email.downcase OR
@@ -29,5 +31,10 @@ class User < ActiveRecord::Base
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
